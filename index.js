0aabc5de695a2c2a93f5cc0e50e2a4a1c6bfa38c
Anmol H R <139048895+AnmolHR23@users.noreply.github.com>
2023-09-15 12:16:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const close = document.querySelector(".close");
+const open = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+close.addEventListener("click", () => {
+  menu.style.visibility = "hidden";
+});
+open.addEventListener("click", () => {
+  menu.style.visibility = "visible";
+});
+
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
